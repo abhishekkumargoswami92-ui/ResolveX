435f558fd55d461a32b9542ede69046019d35ed7
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-25 19:29:39 +0530

    routes and App react file added in frontend

---
+++ client/src/routes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/public/Landing";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
