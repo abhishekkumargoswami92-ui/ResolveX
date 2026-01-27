8d9db63d3bf0d916042937ba59455135f250d467
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-27 21:33:24 +0530

    BackButton fixing

--- client/src/components/common/BackButton.jsx
+++ client/src/components/common/BackButton.jsx
@@ -1,11 +1,27 @@
-<div
-  style={{
-    display: "flex",
-    alignItems: "center",
-    gap: "16px",
-    marginBottom: "10px",
-  }}
->
-  <BackButton />
-  <h1 style={{ margin: 0 }}>Report an Issue</h1>
-</div>
+import { useNavigate } from "react-router-dom";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      onClick={() => navigate(-1)}
+      aria-label="Go back"
+      style={{
+        background: "none",
+        border: "none",
+        padding: 0,
+        margin: 0,
+        cursor: "pointer",
+        fontSize: "1em",
+        fontWeight: 500,
+        lineHeight: "1",
+        color: "inherit",
+      }}
+    >
+      ←
+    </button>
+  );
+};
+
+export default BackButton;
