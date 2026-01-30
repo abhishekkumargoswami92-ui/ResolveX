f3fe08d578af78a68b30ce630d690e4d6c0f5be7
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 02:15:25 +0530

    updated

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const connectDB = require("./config/db");
-const { PORT } = require("./config/env");
+import app from "./app.js";
+import connectDB from "./config/db.js";
+import { PORT } from "./config/env.js";
 
 connectDB();
 
