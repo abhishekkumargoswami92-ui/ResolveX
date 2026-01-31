61ab62299c3515a2d36de609fc035edb045d58ff
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 23:00:06 +0530

    changes temporory

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>
+  // 🔥 REMOVE StrictMode
+  <HashRouter>
+    <AuthProvider>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+    </AuthProvider>
+  </HashRouter>
 );
