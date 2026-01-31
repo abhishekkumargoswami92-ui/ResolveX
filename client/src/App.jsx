5d086c55465f23ef260a4d798cbd97fa61861647
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 23:11:44 +0530

    changes temporory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
-import AppRoutes from "./routes";
-
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <div style={{ padding: "40px", color: "white" }}>
+      <h1 style={{ fontSize: "32px" }}>ResolveX</h1>
+      <p>Campus Issue Tracking System</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <button>Student Dashboard</button>
+        <button style={{ marginLeft: "10px" }}>Admin Dashboard</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <React.Fragment>
     <App />
-  </HashRouter>
+  </React.Fragment>
 );
