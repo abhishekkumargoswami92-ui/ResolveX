8645917e31220d6462f7233387ff0d660602a63c
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 05:35:00 +0530

    updated

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,12 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import { PORT } from "./config/env.js";
 
-connectDB();
+const startServer = async () => {
+  await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
