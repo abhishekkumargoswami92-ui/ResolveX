785adff2ba5f125f4a9d4e8912b3f860f1b91793
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-01-31 22:48:38 +0530

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // ⭐ REQUIRED for Netlify
+  resolve: {
+    dedupe: ["react", "react-dom"],
+  },
 });
